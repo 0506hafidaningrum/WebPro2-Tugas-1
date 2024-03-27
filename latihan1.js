2bd2917371c6cb34897c00234c4ec9efb7143cfb
0506hafidaningrum <ningrumhafida31@gmail.com>
2024-03-27 10:44:55 +0700

    latihan1 webpro2:

---
+++ latihan1.js
@@ -0,0 +1,21 @@
+function checkBB(BB)
+{
+    // TODO: write your code here
+    if (BB > 80)
+    {
+        console.log("gemuk");
+    }
+    
+    else if (BB >= 60 &&  BB <= 80)
+    {
+        console.log("sedang");
+    }
+    
+    else 
+    
+        console.log("kurus");
+    }
+
+checkBB(85);
+checkBB(60);
+checkBB(40);
